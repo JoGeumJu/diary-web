556a3b5d95cf300517a9854a115ef609e40f005e
JoGeumJu <ccho8702@naver.com>
2022-12-30 19:43:51 +0900

    프로젝트 기본 설정

---
+++ src/initialFile.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+
+export const initF: React.FC = () => {
+  return (
+    <Wrapper>
+      <></>
+    </Wrapper>
+  );
+};
+const Wrapper = styled.section`
+  display: flex;
+`;
