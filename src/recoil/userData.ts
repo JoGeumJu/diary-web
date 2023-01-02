f1688191bb7c3c4a25b0b00e3d995ff26b684326
JoGeumJu <ccho8702@naver.com>
2023-01-02 18:14:24 +0900

    userData인 전역변수추가 (todo관련 변수들)

--- src/recoil/userData.ts
+++ src/recoil/userData.ts
@@ -9,7 +9,19 @@ export const ddayDataState = atom<DdayProps[]>({
   key: "dday",
   default: [{ day: "2000-0-0", dday: "Dday-0", content: "샬라샬라" }],
 });
-export const todoDataState = atom({
-  key: "todo",
-  default: [],
+export const todoLDataState = atom<string[]>({
+  key: "todoLData",
+  default: ["샬라샬라"],
+});
+export const todoTDataState = atom<string[]>({
+  key: "todoTData",
+  default: ["샬라샬라"],
+});
+export const todoEDDataState = atom<string[]>({
+  key: "todoEDData",
+  default: ["샬라샬라"],
+});
+export const todoEMDataState = atom<string[]>({
+  key: "todoEMData",
+  default: ["샬라샬라"],
 });
