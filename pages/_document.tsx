3838d27664ba92e84cca8b0f3bbaa8685101197d
JoGeumJu <ccho8702@naver.com>
2023-01-10 18:41:25 +0900

    Head의 title과 shortcutIcon 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta charSet="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no"
+      />
+      <meta name="description" content="Dev.log" />
+      <meta name="keywords" content="blog,react,antd,webpack,css,javascript" />
+      <title>My diary-web</title>
+      <link rel="shortcut icon" href="/img/notebook.png" />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
